735e8d269470dbbab8390ddb5c8ac3edf1b2f262
Wang Yi <njucst@gmail.com>
2013-04-17 12:48:47 +0800

    //////////////////////////////////////
    using namespace std;
    
    enum OptionType { OptionBool, OptionInt, OptionDouble, OptionString };
    enum ParameterType { SIMPLE, INTEGER, FLOAT, STRING, };
    
    void AddOption(const std::string &long_name, const std::string &short_name,
            bool &bool_option, const std::string &description);
    void AddOption(const std::string &long_name, const std::string &short_name,
            int &int_option, const std::string &description);
    void AddOption(const std::string &long_name, const std::string &short_name,
            double &double_option, const std::string &description);
    void AddOption(const std::string &long_name, const std::string &short_name,
            std::string &string_option, const std::string &description);
    
    void ProcessOptions(int &argc, char *argv[]);
    std::string OptionDescriptions();
    
    void AddParameter(const char *name, void *pointer, ParameterType type);
    void ProcessParameters(int &argc, char *argv[]);
    //////////////////////////////////////////////////////////////////////////////////
    long long getFileLine(string filename);

---
+++ Utils.h
@@ -0,0 +1,30 @@
+#ifndef MCH_HYBRID_UTILS_H_
+
+#define MCH_HYBRID_UTILS_H_
+
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+
+enum OptionType { OptionBool, OptionInt, OptionDouble, OptionString };
+enum ParameterType { SIMPLE, INTEGER, FLOAT, STRING, };
+
+void AddOption(const std::string &long_name, const std::string &short_name,
+        bool &bool_option, const std::string &description);
+void AddOption(const std::string &long_name, const std::string &short_name,
+        int &int_option, const std::string &description);
+void AddOption(const std::string &long_name, const std::string &short_name,
+        double &double_option, const std::string &description);
+void AddOption(const std::string &long_name, const std::string &short_name,
+        std::string &string_option, const std::string &description);
+
+void ProcessOptions(int &argc, char *argv[]);
+std::string OptionDescriptions();
+
+void AddParameter(const char *name, void *pointer, ParameterType type);
+void ProcessParameters(int &argc, char *argv[]);
+//////////////////////////////////////////////////////////////////////////////////
+long long getFileLine(string filename);
+
+#endif
